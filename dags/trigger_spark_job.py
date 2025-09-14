07c5a64b1b71ad13d40c2037c9cf51cbcf99b0d1
Nick Peachey <nick.peachey@gmail.com>
2025-09-14 12:48:59 +0100

    s3 file watcher

--- dags/trigger_spark_job.py
+++ dags/trigger_spark_job.py
@@ -1,16 +1,30 @@
 from airflow import DAG
-from datetime import datetime
-from airflow.providers.apache.spark.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
 with DAG(
-    dag_id="trigger_spark_job",
-    schedule_interval=None,
+    dag_id='watch_s3_bucket',
+    default_args=default_args,
+    description='Watch for new files in an S3 bucket',
+    schedule_interval='@hourly',
     start_date=datetime(2024, 1, 1),
     catchup=False,
+    tags=['s3', 'sensor'],
 ) as dag:
-    trigger_spark = SparkKubernetesOperator(
-        task_id="spark_pi",
-        namespace="default",
-        application_file="/opt/airflow/spark-jobs/spark-pi.yaml",
-        do_xcom_push=True,
+
+    watch_for_file = S3KeySensor(
+        task_id='watch_for_file',
+        bucket_key='*',  # Watch for any file in the bucket
+        bucket_name='data-lake',
+        aws_conn_id='minio_conn',
+        poke_interval=60,
+        timeout=60 * 60,
+        mode='poke',
     )
