b61505e075568f1648393d2c07e32f557251a4ea
Nick Peachey <nick.peachey@gmail.com>
2025-09-17 09:21:57 +0100

    updated to newer image

--- dags/test_localstack_dag.py
+++ dags/test_localstack_dag.py
@@ -67,7 +67,7 @@ def generate_spark_minio_config(**kwargs):
             "spec": {
                 "type": "Scala",
                 "mode": "cluster",
-                "image": "nickpeachey/windows-spark-job:2.0.2", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
+                "image": "nickpeachey/windows-spark-job:2.0.3", # IMPORTANT: Replace with your actual Spark image (e.g., with Hadoop S3A support)
                 "imagePullPolicy": "Always",
                 "mainClass": "com.cawooka.windowssparkjob.SparkPi", # IMPORTANT: Replace with your Scala main class
                 "mainApplicationFile": "local:///opt/spark/jars/windows-spark-job.jar", # IMPORTANT: Path to your JAR inside the Spark image
